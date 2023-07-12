8e272087f2211a6dd73dccb5727363af8289e0fc
stephen <skaranja654@gmail.com>
2023-07-12 23:06:43 +0300

    crud on routes

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -31,3 +31,66 @@ class Index(Resource):
         )
         return response
 api.add_resource(Index, "/")
+
+class Users(Resource):
+
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        response = make_response(
+            jsonify(users),
+            200
+            )
+
+        return response
+    
+    def post(self):
+        new_user = User(
+            first_name = request.form['first_name'],
+            last_name = request.form['last_name'],
+            email = request.form['email'],
+            password = request.form['password']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        response_dict = new_user.to_dict()
+        response = make_response(response_dict, 201)
+
+        return response
+api.add_resource(Users, "/users")
+
+class User_by_Id(Resource):
+
+    def get(self, id):
+        response_dict = User.query.filter_by(id=id).first().to_dict()
+        response = make_response(response_dict, 200)
+        
+        return response
+    
+    def patch(self, id):
+        updated_one = User.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(updated_one, attr, request.form[attr])
+
+        db.session.add(updated_one)
+        db.session.commit()
+
+        response_dict = updated_one.to_dict()
+        response = make_response(response_dict, 200)
+
+        return response
+    
+    def delete(self, id):
+        selected_one = User.query.filter_by(id=id).first()
+        
+        db.session.delete(selected_one)
+        db.session.commit()
+
+        response_dict = {"message": "Delete successfull"}
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+        return response
+
+api.add_resource(User_by_Id, "/users/<int:id>")
