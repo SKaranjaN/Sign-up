5fd08e4de7a4368b6a4bec9d8ff40bc5d2de3ff3
stephen <skaranja654@gmail.com>
2023-07-12 23:02:50 +0300

    populated table

--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,28 @@
 from faker import Faker
 from app import app
 from models import db, User
+import re
 
 with app.app_context():
     fake = Faker()
 
     for _ in range(30):
-        user = User(
-            first_name=fake.first_name(),
-            last_name=fake.last_name(),
-            email=fake.email(),
-            password=fake.password(),
-        )
-        db.session.add(user)
+        password = fake.password(
+            length=10,
+            special_chars=True,
+            digits=True,
+            upper_case=True
+        ) 
+
+        try:
+            user = User(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                email=fake.email(),
+                password=password,
+            )
+            db.session.add(user)
+        except ValueError as e:
+            (f"Password validation error: {str(e)}")
 
     db.session.commit()
