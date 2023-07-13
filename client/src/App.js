5f069e2f44e925948585d6bc569957795044319a
stephen <skaranja654@gmail.com>
2023-07-13 14:21:58 +0300

    dashboard and authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import ResetPassword from './components/ResetPassword';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem('token')
+  );
+
+  const handleLogin = (token) => {
+    setIsAuthenticated(true);
+    localStorage.setItem('token', token);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('token');
+  };
+
   return (
     <Router>
       <div className="App">
         <div className="background-image" />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ResetPassword />} />
+          <Route
+            path="/dashboard"
+            element={
+              isAuthenticated ? (
+                <Dashboard onLogout={handleLogout} />
+              ) : (
+                <Login onLogin={handleLogin} />
+              )
+            }
+          />
         </Routes>
       </div>
     </Router>
